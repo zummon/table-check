d6cdec88a9010650601ea0f7c291257571d3d3d6
Teerapat Anantarattanachai <zummon.space@gmail.com>
2023-05-12 17:02:00 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	let keys = [];
+	let array = [];
+</script>
+
+<div class="mx-auto flex max-w-3xl flex-wrap">
+	<div class="w-full p-2">
+		<textarea
+			class="w-full resize-none text-black"
+			rows="10"
+			on:change={(event) => {
+				let result = [];
+				let value = event.target.value;
+				value = value.split("\n");
+				value = value.reduce((accu, str) => {
+					accu.push(str.split("	"));
+					return accu;
+				}, []);
+				value.slice(1).forEach((row) => {
+					let resultSec = {};
+					value[0].forEach((key, index) => {
+						resultSec[key] = row[index];
+					});
+					result.push(resultSec);
+				});
+				array = result;
+			}}
+		/>
+	</div>
+	<div class="w-full p-2">
+		<textarea
+			class="w-full resize-none text-black"
+			rows="10"
+			value={JSON.stringify(array)}
+		/>
+	</div>
+</div>
