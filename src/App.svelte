85a54779cead45b32798576c5780b8822d4b5243
zummon <zummon.space@gmail.com>
2025-02-25 06:59:47 +0700

    up

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,24 @@
 <script>
-	let keys = [];
-	let array = [];
+	let keys = $state([]);
+	let array = $state();
+
+	function handle(event) {
+		let result = [];
+		let value = event.target.value;
+		value = value.split("\n");
+		value = value.reduce((accu, str) => {
+			accu.push(str.split("\t"));
+			return accu;
+		}, []);
+		value.slice(1).forEach((row) => {
+			let resultSec = {};
+			value[0].forEach((key, index) => {
+				resultSec[key] = row[index];
+			});
+			result.push(resultSec);
+		});
+		array = result;
+	}
 </script>
 
 <div class="mx-auto flex max-w-3xl flex-wrap">
@@ -8,30 +26,18 @@
 		<textarea
 			class="w-full resize-none text-black"
 			rows="10"
-			on:change={(event) => {
-				let result = [];
-				let value = event.target.value;
-				value = value.split("\n");
-				value = value.reduce((accu, str) => {
-					accu.push(str.split("	"));
-					return accu;
-				}, []);
-				value.slice(1).forEach((row) => {
-					let resultSec = {};
-					value[0].forEach((key, index) => {
-						resultSec[key] = row[index];
-					});
-					result.push(resultSec);
-				});
-				array = result;
+			placeholder="paste .tsv text here"
+			onchange={(event) => {
+				handle(event);
 			}}
-		/>
+		></textarea>
 	</div>
 	<div class="w-full p-2">
 		<textarea
 			class="w-full resize-none text-black"
 			rows="10"
+			placeholder="array of objects result"
 			value={JSON.stringify(array)}
-		/>
+		></textarea>
 	</div>
 </div>
